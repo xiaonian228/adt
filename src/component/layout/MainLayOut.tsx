5b4fa292223d8f4d7f4e9dde953284b47dfe0ed1
xiaonian228 <xiaonian228@gmail.com>
2024-09-02 20:58:58 +0900

    임시 코드 업데이트

--- src/component/layout/MainLayOut.tsx
+++ src/component/layout/MainLayOut.tsx
@@ -4,10 +4,12 @@ import samplePreview from '../../asset/images/sample/preview.svg'
 import sampleUpNext from '../../asset/images/sample/upnext.svg'
 import sampleCalendar from '../../asset/images/sample/calendar.svg'
 import sampleInfos from '../../asset/images/sample/infos.svg'
-
+import calendar_frame from '../../asset/images/sample/Rectangle.png'
 import preview_bg from '../../asset/images/sample/preview_bg.png'
 import axios from "axios";
 import dayjs from "dayjs";
+import {Current_Month_Layer_Title} from "../../common/dataSet";
+import {AutoTextSize} from "auto-text-size";
 
 
 const MainLayOut = () => {
@@ -47,6 +49,7 @@ const MainLayOut = () => {
     };
 
     React.useEffect(() => {
+        console.log()
         navigator.geolocation.getCurrentPosition((position) => {
             let lat = position.coords.latitude;
             let lon = position.coords.longitude;
@@ -72,30 +75,41 @@ const MainLayOut = () => {
             <div style={{width:'100%', height:'100%', borderRadius:'1.5vw', padding:'1.5vw 2vw', boxSizing:'border-box',
                 backgroundColor:'#0014C1', boxShadow:'0 8px 8px 0 rgba(0,0,0,0.25)'}}>
                 <div style={{fontSize:'3.5vw', fontWeight:700, color:'#fff', marginBottom:'0.8vw', textAlign:'center'}}>{'9월의 안전보건'}</div>
-                <div style={{display:'grid', gap:'0.9375vw'}}>
-                    <div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',
-                        display:'flex',alignItems:'center', justifyContent:'center', verticalAlign:''}}>
-                        {'프레스 산업군 안전사고'}
-                    </div>
-                    <div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',
-                        display:'flex',alignItems:'center', justifyContent:'center'}}>
-                        {'중대재해처벌법 알아보기'}
-                    </div>
-                    <div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',
-                        display:'flex',alignItems:'center', justifyContent:'center'}}>
-                        {'밀폐공간작업을 위한 핵심 수칙'}
-                    </div>
-                    <div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',
-                        display:'flex',alignItems:'center', justifyContent:'center'}}>
-                        {'코로나19 재확산방지 5대수칙'}
-                    </div>
+                <div style={{display:'grid', gap:'0.9375vw', alignItems:'center'}}>
+                    {Current_Month_Layer_Title.slice(5,9).map((v)=>(
+                      <div key={v.key} style={{width:'auto', height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',
+                          display:'flex',alignItems:'center', justifyContent:'center', maxWidth:'calc(100% - 4vw)'}}>
+                          <AutoTextSize maxFontSizePx={54} minFontSizePx={10} style={{lineHeight:'72px'}}>
+                            {v.title}
+                          </AutoTextSize>
+                      </div>
+                    ))}
+
+
+                    {/*<div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',*/}
+                    {/*    display:'flex',alignItems:'center', justifyContent:'center', verticalAlign:''}}>*/}
+                    {/*    {'프레스 산업군 안전사고'}*/}
+                    {/*</div>*/}
+                    {/*<div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',*/}
+                    {/*    display:'flex',alignItems:'center', justifyContent:'center'}}>*/}
+                    {/*    {'중대재해처벌법 알아보기'}*/}
+                    {/*</div>*/}
+                    {/*<div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',*/}
+                    {/*    display:'flex',alignItems:'center', justifyContent:'center'}}>*/}
+                    {/*    {'밀폐공간작업을 위한 핵심 수칙'}*/}
+                    {/*</div>*/}
+                    {/*<div style={{height:'3.75vw', fontSize:'2.8125vw', fontWeight:700, color:'#001f77', borderRadius:'1.5vw', backgroundColor:'#b2b8e8',*/}
+                    {/*    display:'flex',alignItems:'center', justifyContent:'center'}}>*/}
+                    {/*    {'코로나19 재확산방지 5대수칙'}*/}
+                    {/*</div>*/}
                 </div>
             </div>
             <div style={{width:'100%', height:'100%', borderRadius:'1.5vw', padding:'1.2vw 2.5vw', boxSizing:'border-box',
                 backgroundColor:'#0019B1', display:'flex', justifyContent:'center',boxShadow:'0 8px 8px 0 rgba(0,0,0,0.25)'}}>
                 <div style={{fontSize:'6.25vw', fontWeight:500, color:'#fff', maxWidth:'16.66vw', boxSizing:'border-box', textAlign:'right', marginRight:'1.2vw'}}>{dayjs().format('YYYY\nM월\nDD일')}</div>
                 <div style={{backgroundColor:'#000e55', width:'22.29vw', height:'22.29vw', borderRadius:'1.5625vw', color:'#fff'}}>
-                    {'캘린더 API'}
+                    <div style={{display:'flex',justifyContent:'center', alignItems:'center', backgroundRepeat:'no-repeat', width:'100%', height:'100%',
+                        backgroundPosition:'center', backgroundImage:`url('${calendar_frame}')`}} />
                 </div>
             </div>
             <div style={{width:'100%', height:'100%', borderRadius:'1.5vw', boxSizing:'border-box'}}>
@@ -114,8 +128,8 @@ const MainLayOut = () => {
                         </div>
                     </div>
                     <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', width:'22.92vw', height:'22.92vw',borderRadius:'1.5vw', backgroundColor:'#0019B1', color:'#fff'}}>
-                        <div style={{fontSize:'6.25vw', textAlign:'center'}}>
-                            {dayjs().format('hh시 mm분')}
+                        <div style={{fontSize:'6.25vw', textAlign:'center', whiteSpace:'pre-line'}}>
+                            {dayjs().format('hh시\n mm분')}
                         </div>
                         <div style={{fontWeight:300, fontSize:'2.5vw', marginTop:'1.8vw'}}>{dayjs().format('HH:mm:ss')}</div>
                     </div>
